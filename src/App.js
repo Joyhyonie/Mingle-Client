3402c9c43d1aa922d168f8e901b923f78f1ac08e
Joyhyonie <cho970129@gmail.com>
2023-05-11 09:37:02 +0900

    💄 Design: Outlet size 설정

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={ <Layout/> }>
           <Route index element={ <Main/> }/>
+          
         </Route>
         
       </Routes>
