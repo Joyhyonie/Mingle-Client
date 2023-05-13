33636925a215224410f3835ff55e29fd1b88a4c7
manymorehyun <123722508+moreHyun@users.noreply.github.com>
2023-05-13 17:23:57 +0900

    ✨ Feat: 서치바 틀 만들기

--- src/pages/academic/EmployeeManagement.js
+++ src/pages/academic/EmployeeManagement.js
@@ -1,15 +1,33 @@
 /* 행정직원의 '교직원 관리' */
+import React, { useState } from 'react';
 import { motion } from "framer-motion"
+import SelectBox from "../../components/common/SelectBox"
+import SearchBar from "../../components/common/SearchBar";
+import SearchBarCss from '../../css/common/SearchBar.module.css'
+
+
+const OPTIONS = [
+        { value : "empCode", name : "교번" },
+        { value : "empName", name : "직원명" },
+        { value : "deptCode", name : "부서명" },
+];
 
 function EmployeeManagement () {
 
-    return (
-        <motion.div
-            initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ ease: "easeOut", duration: 0.5 }}
-        >
-            교직원 관리 👻
-        </motion.div>
-    );
+  return (
+    <>
+      <motion.div
+          initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ ease: "easeOut", duration: 0.5 }}
+      >
+          교직원 관리 👻
+      </motion.div>
+
+      <div className={SearchBarCss.basic}>
+        <SelectBox options={OPTIONS} defaultValue="deptCode"></SelectBox>
+        <SearchBar></SearchBar>
+      </div>
+    </>
+  );
 }
 
 export default EmployeeManagement;
