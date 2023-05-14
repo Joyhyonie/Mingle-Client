f70a76786244d2070184190bb95ed63e45a2c8fb
manymorehyun <123722508+moreHyun@users.noreply.github.com>
2023-05-15 02:21:45 +0900

    ✨ Feat: CSS오류 심해서 제외하고 form만 추가

--- src/pages/academic/EmployeeManagement.js
+++ src/pages/academic/EmployeeManagement.js
@@ -1,33 +1,65 @@
 /* 행정직원의 '교직원 관리' */
-import React, { useState } from 'react';
-import { motion } from "framer-motion"
-import SelectBox from "../../components/common/SelectBox"
-import SearchBar from "../../components/common/SearchBar";
-import SearchBarCss from '../../css/common/SearchBar.module.css'
+import React, { useEffect, useState, useRef, useMemo } from 'react';
+import { motion } from "framer-motion";
+import axios from 'axios';
+import SearchBarCss from '../../css/common/SearchBar.module.css';
+import SearchAndListLayout from '../../layouts/SearchAndListLayout';
+import OrgCss from '../../css/Org.module.css';
 
-
-const OPTIONS = [
-        { value : "empCode", name : "교번" },
-        { value : "empName", name : "직원명" },
-        { value : "deptCode", name : "부서명" },
+const options = [
+  { value: "empCode", name: "교번" },
+  { value: "empName", name: "직원명" },
+  { value: "deptCode", name: "부서명" },
 ];
 
-function EmployeeManagement () {
+
+function EmployeeManagement() {
 
   return (
-    <>
-      <motion.div
+    <div className={SearchBarCss.container}>
+      <div className={SearchBarCss.menuTitle}>
+        <motion.div
           initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ ease: "easeOut", duration: 0.5 }}
-      >
+        >
           교직원 관리 👻
-      </motion.div>
+        </motion.div>
+      </div>
 
       <div className={SearchBarCss.basic}>
-        <SelectBox options={OPTIONS} defaultValue="deptCode"></SelectBox>
-        <SearchBar></SearchBar>
+        <SearchAndListLayout options={options}></SearchAndListLayout>
       </div>
-    </>
+
+      <table>
+        <thread>
+          <input type="checkbox"></input>
+          <tr>
+            <th>이름</th>
+            <th>교번</th>
+            <th>학과</th>
+            <th>이메일</th>
+            <th>휴대전화</th>
+            <th>입사일</th>
+            <th>상태</th>
+          </tr>
+        </thread>
+        <tbody>
+          <input type="checkbox"></input>
+          <tr>
+            <td>홍길동</td>
+            <td>000001</td>
+            <td>경영학과</td>
+            <td>hong123@mingle.ac.kr</td>
+            <td>010-1234-5678</td>
+            <td>2023-03-12</td>
+            <td>재직</td>
+          </tr>
+        </tbody>
+      </table>
+
+
+
+    </div>
   );
-}
+};
 
 export default EmployeeManagement;
