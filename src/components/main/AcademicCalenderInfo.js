7630efaef0360b09c41614a4a9d801aeaced10e8
Joyhyonie <cho970129@gmail.com>
2023-05-14 14:32:54 +0900

    ✨ Feat: 학사 일정 캘린더 추가

--- src/components/main/AcademicCalenderInfo.js
+++ src/components/main/AcademicCalenderInfo.js
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion"
 import MainCSS from "../../css/Main.module.css"
 
-function AcademicCalenderInfo () {
+function AcademicCalenderInfo ({dateInAcCal}) {
 
     const academicItemClickHandler = () => {
         console.log('학사 일정 상세 모달창 띄우는 함수!')
@@ -15,13 +15,20 @@ function AcademicCalenderInfo () {
     /* (암시용 )학사일정명이 23자 이상일 경우 그 뒤는 ...으로 화면에 노출되게끔 하기 위한 테스트 */
     const text = '2023학년도 1학기 강의 평가 실시 (3학년 우선 실시)';
 
+    /* 클릭된 날짜의 월/일/요일을 변경시키는 이벤트 함수 */
+    const clickedDateHandler = () => {
+        const clickedDate = new Date(dateInAcCal);
+        const days = ['일', '월', '화', '수', '목', '금', '토'];
+        return `${clickedDate.getMonth() + 1}월 ${clickedDate.getDate()}일 ${days[clickedDate.getDay()]}요일`;
+    }
+
     return (
         <motion.div
             initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ ease: "easeOut", duration: 0.5 }}
         >
             <div className={ MainCSS.ScheduleInfoBox }>
                 <div className={ MainCSS.dateBox }>
-                    <p>5월 12일 금요일</p>
+                    <p>{clickedDateHandler()}</p>
                 </div>
                 <div className={ MainCSS.academicItemList }>
                     <div className={ MainCSS.academicItemBox }>
