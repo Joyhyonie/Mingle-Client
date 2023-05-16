6d16f144491dd157f461619f4bca840d53e0e8ab
choijiwon <zinndryke7@gmail.com>
2023-05-16 14:25:28 +0900

    ✨ Feat: 새로운 기능 추가(로그인)

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,10 +1,10 @@
 import { combineReducers } from "redux";
-import EmployeeModule from "./EmployeeModule";
+import employeeReducer from "../modules/EmployeeModule"
 import subjectReducer from "./SubjectModule";
 
 /* 여러 모듈을 combine 시키기 */
 const rootReducer = combineReducers({
-  subjectReducer
+  subjectReducer, employeeReducer
 });
 
 export default rootReducer;
