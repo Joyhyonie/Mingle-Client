946bb555c8eb0d5a26dbb3ee03e3647db591f3ab
Joyhyonie <cho970129@gmail.com>
2023-05-18 00:16:33 +0900

    ✨ Feat: 출퇴근 기록 modal 추가

---
+++ src/components/modal/RecordStartTimeModal.js
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react";
+import MainCSS from "../../css/Main.module.css"
+import { toast } from "react-hot-toast";
+import { useDispatch } from "react-redux";
+
+function RecordStartTimeModal ({setRecordStartTimeModal}) {
+
+    const dispatch = useDispatch();
+
+    /* 등록 성공 시 실행 될 useEffect */
+    // useEffect(
+    //     () => {
+    //         if(recordStartTime?.status === 200) {
+    //             toast.success("오늘의 출근 시각이 등록되었습니다");
+    //             setRecordStartTimeModal(false);
+    //         } 
+    //     }, [recordStartTime]
+    // );
+
+    const timeNow = () => {
+        const now = new Date();
+        const hours = now.getHours();
+        const minutes = now.getMinutes();
+        const ampm = hours < 12 ? '오전' : '오후';
+        const formattedHours = hours % 12 === 0 ? 12 : hours % 12;
+        const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+        return `${ampm} ${formattedHours}시 ${formattedMinutes}분`;
+    } 
+
+    const recordStartTimeHandler = () => {
+        // 출근 시각을 등록하기 위한 API
+        // dispatch(callStartTimeInsertAPI());
+
+        // 임시용
+        toast.success("오늘의 출근 시각이 등록되었습니다");
+        setRecordStartTimeModal(false);
+    }
+
+    return (
+        <div className={ MainCSS.modal } onClick={ () => setRecordStartTimeModal(false)}>
+            <div className={ MainCSS.attendanceModalContainer } onClick={ (e) => e.stopPropagation() }>
+                <p className={ MainCSS.timeNow }>{ timeNow() }</p>
+                <p>오늘의 출근 시각을 등록하시겠습니까?</p>
+                <div className={ MainCSS.centerButtonBox }>
+                    <button className={ MainCSS.whiteButton } onClick={ () => setRecordStartTimeModal(false)}>
+                        취소
+                    </button>
+                    <button className={ MainCSS.pinkButton } onClick={ recordStartTimeHandler }>
+                        등록
+                    </button>
+                </div>
+                <text>MINGLE과 함께 힘찬 하루 보내세요!</text>
+            </div>
+        </div>
+    );
+}
+
+export default RecordStartTimeModal;
