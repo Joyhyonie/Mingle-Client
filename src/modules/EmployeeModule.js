9a4e25676915d51bde69a8ec3f0827e8a2e14d7e
manymorehyun <123722508+moreHyun@users.noreply.github.com>
2023-05-17 09:06:00 +0900

    ✨ Feat: 교직원 조회

--- src/modules/EmployeeModule.js
+++ src/modules/EmployeeModule.js
@@ -7,21 +7,29 @@ const initialState = {};
 /* 액션 */
 
 const POST_LOGIN = 'employee/POST_LOGIN';
-const GET_EMPLOYEE = 'employee/GET_EMPLOYEE'
+const RESET_EMPLOYEE = 'employee/RESET_MEMBER';
+const GET_EMPLOYEES = 'employee/GET_EMPLOYEES';
+const GET_EMPLOYEE = 'employee/GET_EMPLOYEE';
 
-export const { employee : { postLogin, resetEmployee, getEmployee }} = createActions({
+export const { employee : 
+  { postLogin, resetEmployee, getEmployees, getEmployee }} = createActions
+  ({
 
     [POST_LOGIN] : res => res,
+    [RESET_EMPLOYEE] : () => {},
+    [GET_EMPLOYEES] : (res) => res.data,
     [GET_EMPLOYEE] : res => res
 
 });
 
 /* 리듀서 */
-const employeeReducer = handleActions({
+const EmployeeReducer = handleActions({
     [POST_LOGIN] : (state, {payload}) => ({ login : payload}),
+    [RESET_EMPLOYEE] : (state, {payload}) => initialState,
+    [GET_EMPLOYEES] : (state, {payload}) => payload,
     [GET_EMPLOYEE] : (state, {payload}) => ({employee : payload})
 
 
 }, initialState);
 
-export default employeeReducer;
+export default EmployeeReducer;
