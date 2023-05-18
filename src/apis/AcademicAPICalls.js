9f11016ebfe0ad792da515248d374c2e83237b25
manymorehyun <123722508+moreHyun@users.noreply.github.com>
2023-05-18 17:06:06 +0900

    ✨ Feat: 조직도 교직원 조회

--- src/apis/AcademicAPICalls.js
+++ src/apis/AcademicAPICalls.js
@@ -1,9 +1,12 @@
-import { getEmployees } from '../modules/EmployeeModule';
+import { getEmployees, getEmployee, postEmployee, putEmployee } from '../modules/EmployeeModule';
+import { getStudents } from '../modules/StudentModule';
 
 const SERVER_IP = `${process.env.REACT_APP_RESTAPI_SERVER_IP}`;
 const SERVER_PORT = `${process.env.REACT_APP_RESTAPI_SERVER_PORT}`;
 const EMPLOYEE_URL = `http://${SERVER_IP}:${SERVER_PORT}/employee`;
+const STUDENT_URL = `http://${SERVER_IP}:${SERVER_PORT}/student`;
 
+// 교직원 전체 조회
 export const callEmployeesAPI = ({ currentPage = 1 }) => {
 
   const requestURL = `${EMPLOYEE_URL}/employees?page=${currentPage}`;
@@ -20,15 +23,79 @@ export const callEmployeesAPI = ({ currentPage = 1 }) => {
   }
 }
 
-// export const callEmployeeUpdateAPI = ({ deptCode, currentPage = 1 }) => {
+// 교직원 상세 조회
+export const callEmployeeDetailAPI = ({ empCode }) => {
 
-//   const requestURL = `${PRE_URL}/department/{deptCode}?page=${currentPage}`;
+  const requestURL = `${EMPLOYEE_URL}/employees/{empCode}`;
 
-//   return async (dispatch, getState) => {
+  return async (dispatch, getState) => {
 
-//     const result = await fetch(requestURL).then(response => response.json);
+    const result = await fetch(requestURL).then(response => response.json());
 
-//     if (result.status === 200) {
-//     }
-//   }
-// }
+    if (result.status === 200) {
+      console.log("[AcademicAPICalls] callEmployeeDetailAPI result : ", result);
+      dispatch(getEmployee(result));
+
+
+    }
+  }
+
+}
+
+// 교직원 신규 등록
+export const callEmployeeInsertAPI = (formData) => {
+
+  const requestURL = `${EMPLOYEE_URL}/insert`;
+
+  return async (dispatch, getState) => {
+
+    console.log(formData);
+    const result = await fetch(requestURL, {
+      method: "POST",
+      body: formData
+    }).then(response => response.json());
+
+    if (result.status === 200) {
+      console.log('[AcademicAPICalls] : callEmployeeInsertAPI result : ', result);
+      dispatch(postEmployee(result));
+    }
+  }
+}
+
+// 교직원 정보 수정
+export const callEmployeeUpdateAPI = (formData) => {
+
+  const requestURL = `${EMPLOYEE_URL}/modify`;
+
+  return async (dispatch, getState) => {
+
+    console.log(formData);
+    const result = await fetch(requestURL, {
+      method: "PUT",
+      body: formData
+    }).then(response => response.json());
+
+    if (result.status === 200) {
+      console.log('[AcademicAPICalls] : callEmployeeUpdateAPI result : ', result);
+      dispatch(putEmployee(result));
+    }
+  }
+}
+
+
+// 학생 전체 조회
+export const callStudentsAPI = ({ currentPage = 1 }) => {
+
+  const requestURL = `${STUDENT_URL}/students?page=${currentPage}`;
+
+  return async (dispatch, getState) => {
+
+    const result = await fetch(requestURL).then(response => response.json());
+    console.log(result);
+
+    if (result.status === 200) {
+      dispatch(getStudents(result));
+      console.log(result);
+    }
+  }
+}
--- src/modules/EmployeeModule.js
+++ src/modules/EmployeeModule.js
@@ -1,4 +1,3 @@
-
 import { createActions, handleActions } from "redux-actions";
 
 /* 초기값 */
@@ -10,16 +9,19 @@ const POST_LOGIN = 'employee/POST_LOGIN';
 const RESET_EMPLOYEE = 'employee/RESET_MEMBER';
 const GET_EMPLOYEES = 'employee/GET_EMPLOYEES';
 const GET_EMPLOYEE = 'employee/GET_EMPLOYEE';
+const POST_EMPLOYEE = 'employee/POST_EMPLOYEE';
+const PUT_EMPLOYEE = 'employee/PUT_EMPLOYEE';
 
 export const { employee : 
-  { postLogin, resetEmployee, getEmployees, getEmployee }} = createActions
+  { postLogin, resetEmployee, getEmployees, getEmployee, postEmployee, putEmployee }} = createActions
   ({
 
     [POST_LOGIN] : res => res,
     [RESET_EMPLOYEE] : () => {},
     [GET_EMPLOYEES] : (res) => res.data,
-    [GET_EMPLOYEE] : res => res
-
+    [GET_EMPLOYEE] : (res) => res.data,
+    [POST_EMPLOYEE] : (res) => res,
+    [PUT_EMPLOYEE] : (res) => res
 });
 
 /* 리듀서 */
@@ -27,8 +29,9 @@ const EmployeeReducer = handleActions({
     [POST_LOGIN] : (state, {payload}) => ({ login : payload}),
     [RESET_EMPLOYEE] : (state, {payload}) => initialState,
     [GET_EMPLOYEES] : (state, {payload}) => payload,
-    [GET_EMPLOYEE] : (state, {payload}) => ({employee : payload})
-
+    [GET_EMPLOYEE] : (state, {payload}) => payload,
+    [POST_EMPLOYEE] : (state, {payload}) => ({regist : payload}),
+    [PUT_EMPLOYEE] : (state, {payload}) => ({modify : payload})
 
 }, initialState);
 
