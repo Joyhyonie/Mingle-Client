1ba6b3342dbb3a73ffd2a72abba888def0f7b54c
Joyhyonie <cho970129@gmail.com>
2023-05-24 17:24:51 +0900

    ✨ Feat: 로그인한 유저에 따른 Header&Navbar 설정 완료

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -2,20 +2,21 @@ import { useEffect, useState } from 'react';
 import Header from '../components/common/Header';
 import NavbarForAdmin from '../components/common/NavbarForAdmin';
 import CommonCSS from '../css/common/Common.module.css';
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import NavbarForProfessor from '../components/common/NavbarForProfessor';
 import { toast } from 'react-hot-toast';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { callGetEmployeeAPI } from '../apis/EmployeeAPICalls';
 
 function Layout () {
 
     const dispatch = useDispatch();
-    const navigate = useNavigate();
+    const { employee } = useSelector(state => state.EmployeeReducer);
     const [activeIndex, setActiveIndex] = useState();   // 활성화된 화면을 컨트롤하기 위한 state
     const [isDark, setIsDark] = useState(false);        // 다크모드 설정을 위한 state
 
-    const isAdmin = true; // (임시용) 로그인한 유저가 행정직원 or 교수인지 판별
+    // 현재 로그인 한 유저가 교수 or 행정직원인지에 따라 Navbar 변경하기 위한 변수
+    const isAdmin = employee && employee.empId.startsWith('AD');
 
     /* 첫 로그인 시, empCode를 찾지 못 해 발생하는 모든 오류를 해결하기 위해 현재 로그인한 유저 조회 API 호출을 Layout에서 실행 */
     useEffect(
@@ -28,7 +29,6 @@ function Layout () {
     const logoutHandler = () => {
         window.localStorage.removeItem('accessToken');
         toast.success('로그아웃 성공 !');
-        // navigate('/login', { replace : true });
         window.location.href = '/login';
     }
 
