c23c3ac6b76b7061f7d79406abb0694d59810499
프리젠트리 <hyunje6334@gmail.com>
2023-05-25 17:19:56 +0900

    ✨ Feat: 강의별 출석/성적 화면 추가

--- src/components/lists/AttendanceList.js
+++ src/components/lists/AttendanceList.js
@@ -2,55 +2,43 @@ import BoardCSS from '../../css/Board.module.css';
 import { motion } from "framer-motion"
 import LectureItem from '../items/LectureItem';
 
-function AttendanceList({LectureInfoList}) {//매개객체 변수가 api에 넣을 것인가?
+function AttendanceList({ LectureInfoList }) {//매개객체 변수가 api에 넣을 것인가?
 
-console.log("AttendanceList :",LectureInfoList);
-return( <motion.div
-    initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ ease: "easeOut", duration: 0.5 }}
->   
-<table className={ BoardCSS.boardTable }>
-                <colgroup>
-                    <col width="10%"/>
-                    <col width="8%"/>
-                    <col width="40%"/>
-                    <col width="10%"/>
-                    <col width="10%"/>
-                    <col width="10%"/>
-                </colgroup>
-                <thead>
-                    <tr>
-                        <th>NO</th>
-                        <th>강의번호</th>
-                        <th>강의명</th>
-                        <th>기간</th>
-                        <th>담임 교수</th>
-                        <th>출석 및 성적 조회</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    { Array.isArray(LectureInfoList)
-                    && LectureInfoList.map(lecture => <LectureItem key={ lecture.lectureCode } lecture={ lecture }/>) 
-                    }
+    console.log("AttendanceList :", LectureInfoList);
+    return (<motion.div
+        initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ ease: "easeOut", duration: 0.5 }}
+    >
+        <table className={BoardCSS.boardTable}>
+            <colgroup>
 
-                    {/* 아래는 dummy ----------------------------------------------------------- */}
+                <col width="10%" />
+                <col width="35%" />
+                <col width="10%" />
+                <col width="10%" />
+                <col width="10%" />
+            </colgroup>
+            <thead>
+                <tr>
 
-                    { Array.isArray(LectureInfoList)
-                    && LectureInfoList.map(lecture => <LectureItem key={ lecture.lectureCode } lecture={ lecture }/>) 
-                    }
-                    { Array.isArray(LectureInfoList)
-                    && LectureInfoList.map(lecture => <LectureItem key={ lecture.lectureCode } lecture={ lecture }/>) 
-                    }
-                    { Array.isArray(LectureInfoList)
-                    && LectureInfoList.map(lecture => <LectureItem key={ lecture.lectureCode } lecture={ lecture }/>) 
-                    }
-                    { Array.isArray(LectureInfoList)
-                    && LectureInfoList.map(lecture => <LectureItem key={ lecture.lectureCode } lecture={ lecture }/>) 
-                    }
-                </tbody>
-            </table>
+                    <th>강의번호</th>
+                    <th>강의명</th>
+                    <th>기간</th>
+                    <th>담임 교수</th>
+                    <th>출석 및 성적 조회</th>
+                </tr>
+            </thead>
+            <tbody>
+                {Array.isArray(LectureInfoList)
+                    && LectureInfoList.map(lecture => <LectureItem key={lecture.lecCode} lecture={lecture} />)
+                }
 
 
-</motion.div>
+
+            </tbody>
+        </table>
+
+
+    </motion.div>
     );
 }
 
